6546dce46783beefe0a0660b8820dd5f3616ed44
MatthewG <mgrimalovsky@gmail.com>
2024-08-14 14:35:30 -0400

    Added debouncer to improve performance; fixed conda history bug

--- src/watcher.ts
+++ src/watcher.ts
@@ -14,7 +14,7 @@ const options = {
     ignored: '^(?!.*(history|__pycache__)).*$',
 };
 const watcher = chokidar.watch([], options);
-export async function watchEnv(env_path: string) {
+export async function watchEnv(DEBOUNCE_DELAY: number, env_path: string) {
     // Upon watching a new conda environment, check for initial updates
     const new_env_content = await exportEnv(env_path);
     await updateEnv(new_env_content);
@@ -26,21 +26,42 @@ export async function watchEnv(env_path: string) {
     const pip_path = path.join(env_path, 'Lib', 'site-packages');
     watcher.add([history_path, pip_path]);
 
-    watcher
-        .on('addDir', async (file_path) => {
+    let debounceTimeout : NodeJS.Timeout;
+    const debounceUpdate = (delay = DEBOUNCE_DELAY) => { // avoids overuse of updateEnv, which calls python.exe and cmd.exe (see utils.ts)
+        clearTimeout(debounceTimeout);
+        debounceTimeout = setTimeout(async () => {
             try {
                 const output = await exportEnv(env_path);
                 await updateEnv(output);
             } catch (error) {
                 vscode.window.showErrorMessage(`CondaSync: Failed to export Conda environment: ${error}`);
             }
+        }, delay);
+    };
+
+    watcher
+        .on('change', async (file_path) => {
+            if (file_path === history_path) {
+                //vscode.window.showInformationMessage(`Changed: ${file_path}`);
+                debounceUpdate();
+            }
+        })
+        .on('addDir', async (file_path) => {
+            //vscode.window.showInformationMessage(`Added Dir: ${file_path}`);
+            debounceUpdate();
         })
         .on('unlinkDir', async (file_path) => {
-            try {
-                const output = await exportEnv(env_path);
-                await updateEnv(output);
-            } catch (error) {
-                vscode.window.showErrorMessage(`CondaSync: Failed to export Conda environment: ${error}`);
+            //vscode.window.showInformationMessage(`Unliked Dir: ${file_path}`);
+            debounceUpdate();
+        })
+        .on('unlink', async (file_path) => {
+            if (file_path === history_path) {
+                try {
+                    //vscode.window.showInformationMessage(`Unliked file: ${file_path}`);
+                    vscode.window.showInformationMessage('CondaSync: Environment removed. Please set new conda environment.');
+                } catch (error) {
+                    vscode.window.showErrorMessage(`CondaSync: Failed to close watcher: ${error}`);
+                }
             }
         });
 }
